e51d31cc2e5c4a6c310edbb2c04c74b65cb2d840
hamoudih1 <hhamoudih1@gmail.com>
2019-08-14 18:11:05 -0400

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
